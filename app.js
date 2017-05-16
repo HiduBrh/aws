066895b06d0b5a00ed9bd4888191057940e4df0d
hidu <mhamed.bourouh@gmail.com>
2017-05-16 14:59:45 +0200

    first commit

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
+var AWS = require('aws-sdk');
 var express = require('express'), http = require('http');
 var app = express();
 
 
-
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database : process.env.RDS_DB_NAME
+AWS.config.update({
+	region: process.env.DYNAMO_REGION,
+	endpoint: process.env.DYNAMO_ENDPOINT
 });
 
 app.get('/', function (req, res) {
-    res.send('Wow');
+	res.send('Wow');
 });
 
 app.get('/students', function (req, res) {
-	connection.connect(function(err) {
-	if (err) {
-		console.error('Database connection failed: ' + err.stack);
-		res.send('Database connection failed: ' + err.stack);
-		return;
-	}});
-	console.log('Connected to database.');
+	
 
-	connection.query('SELECT * FROM students', function (error, results, fields) {
-		connection.end();
-		if (results !== undefined) {
-			res.send(results);
-		} else {
-			res.send(error);
-		}
-	});
 });
 
+app.get('/user/:userId', function(req, res) {
+	var docClient = new AWS.DynamoDB.DocumentClient();
+	var params = {
+		TableName: "users",
+		Key: {
+			"userId": req.params["userId"];
+		}
+	}
+	docClient.get(params, function(err,data) {
+		if(err) {
+			res.send("Error while reading: " + JSON.stringify(err, nulln2));
+		} else {
+			res.send("Get item: " + JSON.stringify(data, null, 2));
+		}
+	});
+})
+
 app.listen(process.env.PORT, function () {
-    console.log("Eventually server successfully started!");
+	console.log("server successfully started!");
 });
